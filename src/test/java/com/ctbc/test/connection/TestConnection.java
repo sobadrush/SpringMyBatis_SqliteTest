712e325c6056a7ecffc5ac99cfb3e99ec1d5a5c8
TizzyBac <TizzyBac TizzyBac@icloud.com>
2018-06-06 20:42:08 +0800

    修改 TestConnection 的 @ActiveProfiles(profiles = { "sqlite_env" })

--- src/test/java/com/ctbc/test/connection/TestConnection.java
+++ src/test/java/com/ctbc/test/connection/TestConnection.java
@@ -14,24 +14,29 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import _01_Config.RootConfig;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @FixMethodOrder(value = MethodSorters.NAME_ASCENDING)
 @ContextConfiguration(classes = { RootConfig.class })
+@Transactional
+@ActiveProfiles(profiles = { "sqlite_env" })
 public class TestConnection {
 
 	private static int testNum = 1;
-	
+
 	@Autowired
 	private DataSource ds;
-	
+
 	@Autowired
 	private SqlSessionFactory sqlSessionFactory;
-	
+
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	}
@@ -43,7 +48,9 @@ public class TestConnection {
 	@Before
 	public void setUp() throws Exception {
 		String testNumStr = String.format("%03d", testNum++);
-		System.out.println(" ==================== " + "test_" + testNumStr + " ==================== ");
+		System.err.println("========================================================= ");
+		System.err.println(" ======================== " + "test_" + testNumStr + " ======================= ");
+		System.err.println(" ========================================================= ");
 	}
 
 	@After
@@ -52,13 +59,15 @@ public class TestConnection {
 
 	@Test
 //	@Ignore
+	@Rollback(true)
 	public void test_001() throws SQLException {
 		String productName = ds.getConnection().getMetaData().getDatabaseProductName();
 		System.err.println(" 資料庫廠商 >>> " + productName);
 	}
-	
+
 	@Test
 //	@Ignore
+	@Rollback(true)
 	public void test_002() throws SQLException {
 		System.err.println(" sqlSessionFactory >>> " + sqlSessionFactory);
 	}
